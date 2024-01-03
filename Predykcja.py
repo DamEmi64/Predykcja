30368b05769527113ebe0108cf65143263dc83fb
DamEmi64 <Damian.janusiak@hotmail.com>
2024-01-03 20:00:57 +0100

    v0.1

--- Predykcja.py
+++ Predykcja.py
@@ -1,49 +1,54 @@
+from numpy.lib import save
 import sklearn.datasets
-from sklearn.neural_network import MLPClassifier
+from sklearn.neural_network import MLPClassifier, MLPRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt  
 # Za³aduj dane
 # Przygotuj dane do uczenia
 # np. X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 def readData():
-    data = sklearn.datasets.load_files('./data/dataset')
+    # przerób
+# zczytuje 2 i 3 jako wez³y 
+# zczytuje dae do linijki
+
+    datafile =  open("./data/dataset/1.txt")
     x = []
-    y = data.target
-    for item in data.data:
-        lines = item.splitlines()
-        numbers = []
-        for line in lines:
-                try:
-                    numbers.append(float(line))
-                except :
-                    pass
-        x.append(numbers)
+    y = []
+    data = datafile.readlines()
+    for idx in range(3,len(data)):  
+            try:            
+                y.append(float(data[idx]))
+                x.append(float(data[idx - 1]))
+            except :
+                pass
 
     return train_test_split(x, y, train_size=0.7)
 
 X_train, X_test, y_train, y_test = readData();
 
-data = []
-for index in range(len(X_train)):
-     data.append([X_train[index],y_train[index]])
 
-# Przeskaluj dane
-scaler = StandardScaler()
-scaler.fit(data)
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
 
 # Utwórz model GNN
-model = MLPClassifier(hidden_layer_sizes=(100, 50), max_iter=1000)
+model = MLPRegressor(hidden_layer_sizes=(100, 50), max_iter=1000)
 
 # Trenuj model
-model.fit(X_train, y_train)
+model.fit(np.array(X_train).reshape(-1,1),y_train)
 
 # Dokonaj predykcji
-y_pred = model.predict(X_test)
+y_pred = model.predict(np.array(X_test).reshape(-1,1))
 
-# Ocena modelu
-accuracy = accuracy_score(y_test, y_pred)
-print("dokladnosc modelu" + accuracy)
+data = []
+for idx in range(0,len(X_test)):
+     data.append([idx,y_test[idx]])
+
+pd.DataFrame(data).to_csv("./data/csv/1_test.csv")
+data = []
+for idx in range(0,len(X_test)):
+     data.append([idx,y_pred[idx]])
+
+pd.DataFrame(data).to_csv("./data/csv/1_pred.csv")
+input()

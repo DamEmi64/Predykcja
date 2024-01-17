4614d99e6fa7eb2bab83f88151d625c672607ee2
DamEmi64 <Damian.janusiak@hotmail.com>
2024-01-17 23:08:21 +0100

    simple

--- example.py
+++ example.py
@@ -1,56 +1,60 @@
-﻿import igraph as ig
-import leidenalg
-import os
+﻿from numpy.lib import save
+import sklearn.datasets
+from sklearn.neural_network import MLPClassifier, MLPRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt  
+import os
 
-# Tworzenie grafu
-g = ig.Graph()
-
-def add_vertex_if_not_exists(graph, vertex_name):
-    if not has_node(graph,vertex_name):
-        graph.add_vertex(vertex_name)
-
-def has_node(graph, name):
-    try:
-        graph.vs.find(name=name)
-    except:
-        return False
-    return True
-def readData():
-    # przer�b
-# zczytuje 2 i 3 jako wez�y 
-# zczytuje dae do linijki
+def find_via_nodes(start_node,end_node):
     directory = './data/dataset'
-    g = ig.Graph();
-    edges = []
-    attributes = []
-    buffer = []
     for filename in os.listdir(directory):
-        if os.path.isfile(os.path.join(directory, filename)) and len(edges)< 150:
+        if os.path.isfile(os.path.join(directory, filename)):
             file = open(os.path.join(directory, filename))
             lines = file.readlines()
-            add_vertex_if_not_exists(g,lines[1])
-            add_vertex_if_not_exists(g,lines[2])
-            edges.append([lines[1],lines[2]])
-            buffer.clear()
+            if int(lines[1]) == start_node and int(lines[2]) == end_node:
+                return  os.path.join(directory, filename)
             
-            for idx in range(3,len(lines)):            
-                try:
-                    buffer.append(float(lines[idx]))
-                except:
-                    pass
-            attributes.append(np.mean(buffer))
-    
-    g.add_edges(edges,{"mean":attributes})
-    return g
 
-g = readData()
+def readData(directory):
+
+    datafile =  open(directory)
+    x = []
+    y = []
+    data = datafile.readlines()
+    for idx in range(3,len(data)):  
+            try:            
+                y.append(float(data[idx]))
+                x.append(float(data[idx - 1]))
+            except :
+                pass
+
+    return train_test_split(x, y, train_size=0.7)
 
 
-# Wykrywanie społeczności przy użyciu algorytmu Leiden
-partition = leidenalg.find_partition(g, leidenalg.ModularityVertexPartition)
+X_train, X_test, y_train, y_test = readData(find_via_nodes(0,1))
 
-# Wyświetlanie wyników
-print("Społeczności:")
-for community in partition:
-    print(community)
+
+
+# Utwórz model GNN
+model = MLPRegressor(hidden_layer_sizes=(100, 50), max_iter=1000)
+
+# Trenuj model
+model.fit(np.array(X_train).reshape(-1,1),y_train)
+
+# Dokonaj predykcji
+y_pred = model.predict(np.array(X_test).reshape(-1,1))
+
+data = []
+for idx in range(0,len(X_test)):
+     data.append([idx,y_test[idx]])
+
+pd.DataFrame(data).to_csv("./data/csv/1_test.csv")
+data = []
+for idx in range(0,len(X_test)):
+     data.append([idx,y_pred[idx]])
+
+pd.DataFrame(data).to_csv("./data/csv/1_pred.csv")
